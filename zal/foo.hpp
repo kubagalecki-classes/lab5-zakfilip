177593dbcbec9f8a783696dbe1223298ccc42b0e
zakfilip <70323056+zakfilip@users.noreply.github.com>
2020-12-07 19:16:53 +0100

    Update foo.hpp

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -11,8 +11,8 @@ std::vector< char > foo(std::list< Human >& people)
     for(auto it = people.rbegin(); it!=people.rend(); --it)
     {
         it->birthday();
-        if(it->isMonster()) tmp.push_back('n');
-        else tmp.push_back('y');
+        if(it->isMonster()) tmp.emplace_back('n');
+        else tmp.emplace_back('y');
     }
     return tmp;
 }
